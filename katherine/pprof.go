45b8bee77d56f6fd975de73f34a4e652f4c4bdaa
justin <pulchritude007@163.com>
2022-04-11 17:14:15 +0800

    pref

---
+++ katherine/pprof.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+func main() {
+	log.SetFlags(log.Ltime | log.LUTC)
+	log.SetOutput(os.Stdout)
+
+	go func() {
+		goroutines := pprof.Lookup("goroutine")
+		for range time.Tick(time.Second) {
+			log.Printf("goroutine count: %d\n", goroutines.Count())
+		}
+	}()
+
+	var blockForever chan struct{}
+	for i := 0; i < 10; i++ {
+		go func() {
+			<-blockForever
+		}()
+		time.Sleep(500 * time.Millisecond)
+	}
+}
